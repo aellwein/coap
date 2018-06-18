48d6c24e4dcf9ac12e5308b2f7da2d66523f96c3
Alex Ellwein <alex@etero.net>
2018-06-18 18:56:20 +0200

    more tests

--- message/message_test.go
+++ message/message_test.go
@@ -3,7 +3,6 @@ package message
 import (
 	. "github.com/smartystreets/goconvey/convey"
 	"testing"
-	"encoding/binary"
 )
 
 func TestTooShortMessageCausesError(t *testing.T) {
@@ -22,8 +21,7 @@ func TestTooShortMessageCausesError(t *testing.T) {
 
 func TestInvalidMessageVersionCausesError(t *testing.T) {
 	Convey("Given a message with an invalid version", t, func() {
-		b := make([]byte, 4)
-		binary.LittleEndian.PutUint32(b, 0x1337)
+		b := []byte{0xCA, 0xFE, 0xBA, 0xBE}
 
 		Convey("When decoded", func() {
 			_, err := Decode(b, nil)
@@ -32,5 +30,22 @@ func TestInvalidMessageVersionCausesError(t *testing.T) {
 				So(err, ShouldEqual, InvalidMessageVersion)
 			})
 		})
+	})
+}
+
+func TestMessageCodeIsParsedCorrectly(t *testing.T) {
+	Convey("Given a message with code class ", t, func() {
+		b := []byte{0x48, 0x02, 0x22, 0x72, 0x04, 0x71, 0xbd, 0x4a, 0xf3, 0xa3, 0x47, 0x09}
+
+		Convey("When decoded", func() {
+			msg, err := Decode(b, nil)
+
+			Convey("Code should be 0.2", func() {
+				So(err, ShouldBeNil)
+				So(msg.Code.CodeClass, ShouldEqual, 0)
+				So(msg.Code.CodeDetail, ShouldEqual, 2)
+			})
+		})
+
 	})
 }
