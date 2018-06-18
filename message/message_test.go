2096acf8e32722af42cfe87a05fe9f154cbe156d
Alex Ellwein <alex@etero.net>
2018-06-18 19:16:02 +0200

    test for message which token is too short

--- message/message_test.go
+++ message/message_test.go
@@ -47,6 +47,20 @@ func TestInvalidTokenLength(t *testing.T) {
 	})
 }
 
+func TestMessageIsCorruptedOnToken(t *testing.T) {
+	Convey("Given a message with valid token length", t, func() {
+		b := []byte{0x48, 0x02, 0x22, 0x72, 0x04, 0x71, 0xbd, 0x4a, 0xf3}
+
+		Convey("When decoded", func() {
+			_, err := Decode(b, nil)
+
+			Convey("'Packet Is Too Short' is indicated by error", func() {
+				So(err, ShouldEqual, PacketIsTooShort)
+			})
+		})
+	})
+}
+
 func TestMessageCodeIsParsedCorrectly(t *testing.T) {
 	Convey("Given a message with code class 0.2", t, func() {
 		b := []byte{0x48, 0x02, 0x22, 0x72, 0x04, 0x71, 0xbd, 0x4a, 0xf3, 0xa3, 0x47, 0x09}
