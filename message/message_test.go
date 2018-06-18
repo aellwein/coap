0b679ca344b370b05e670997e525062aa7637d54
Alex Ellwein <alex@etero.net>
2018-06-18 18:07:01 +0200

    add some tests

--- message/message_test.go
+++ message/message_test.go
@@ -1,12 +1,36 @@
 package message
 
 import (
-	"github.com/smartystreets/goconvey/convey"
+	. "github.com/smartystreets/goconvey/convey"
 	"testing"
+	"encoding/binary"
 )
 
-func TestSomething(t *testing.T) {
-	convey.Convey("everything is great", t, func() {
-		t.Log("All super")
+func TestTooShortMessageCausesError(t *testing.T) {
+	Convey("Given a too short message", t, func() {
+		b := make([]byte, 2)
+
+		Convey("When decoded", func() {
+			_, err := Decode(b, nil)
+
+			Convey("'Packet is too short' is indicated by error", func() {
+				So(err, ShouldEqual, PacketIsTooShort)
+			})
+		})
+	})
+}
+
+func TestInvalidMessageVersionCausesError(t *testing.T) {
+	Convey("Given a message with an invalid version", t, func() {
+		b := make([]byte, 4)
+		binary.LittleEndian.PutUint32(b, 0x1337)
+
+		Convey("When decoded", func() {
+			_, err := Decode(b, nil)
+
+			Convey("'Invalid Message Version' is indicated by error", func() {
+				So(err, ShouldEqual, InvalidMessageVersion)
+			})
+		})
 	})
 }
