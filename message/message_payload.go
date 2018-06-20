7e71313b4ded18a4e3ee79dc321be2f7e3b31324
Alex Ellwein <alex@etero.net>
2018-06-20 20:44:42 +0200

    payload String() is a hex dump

--- message/message_payload.go
+++ message/message_payload.go
@@ -1,22 +1,7 @@
 package message
 
-import (
-	"bytes"
-	"fmt"
-)
-
 type PayloadType []byte
 
 func (p PayloadType) String() string {
-	var b bytes.Buffer
-	b.WriteString("[")
-	for n, i := range p {
-		if n == 0 {
-			b.WriteString(fmt.Sprintf("0x%02X", i))
-		} else {
-			b.WriteString(fmt.Sprintf(", 0x%02X", i))
-		}
-	}
-	b.WriteString("]")
-	return b.String()
+	return HexContent(p)
 }
