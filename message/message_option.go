3d4cf088eaded623e0ee3084f2457bead4c72562
Alex Ellwein <alex@etero.net>
2018-06-20 17:51:11 +0200

    fix option parser, stringify token in HEX

--- message/message_option.go
+++ message/message_option.go
@@ -2,8 +2,6 @@ package message
 
 import (
 	"encoding/binary"
-	"encoding/hex"
-	"fmt"
 )
 
 // option number is in uint16 range
@@ -16,86 +14,82 @@ type OptionValueType []byte
 // OptionNumber : [ OptionValue1, OptionValue2, ...OptionValueN ]
 type OptionsType map[OptionNumberType][]OptionValueType
 
-// decode option from message buffer and return the rest of the buffer, if any
-func decodeOptions(options *OptionsType, buffer []byte) error {
+// decode option from message buffer and return the next position in the buffer
+func decodeOptions(options *OptionsType, buffer []byte) (int, error) {
 	var (
 		optionDelta  int
 		optionLength int
 		optionValue  []byte
 	)
-	if len(buffer) > 0 {
-		i := 0
-		for {
-			od := buffer[i] >> 4
-			ol := buffer[i] & 0xF
+	i := 0
+	for len(buffer) > i {
+		od := buffer[i] >> 4
+		ol := buffer[i] & 0xF
 
-			// figure out Option Delta
-			switch od {
-
-			case 13:
-				i++
-				if len(buffer) < i+1 {
-					return PacketIsTooShort
-				}
-				optionDelta += int(buffer[i])
-				i++
-
-			case 14:
-				i++
-				if len(buffer) < i+2 {
-					return PacketIsTooShort
-				}
-				optionDelta += int(binary.BigEndian.Uint16(buffer[i:i+2]) + 269)
-				i += 2
-
-			case 15:
-				// whole byte should be 0xFF, otherwise error
-				if ol != 0xF {
-					return MessageFormatError
-				} else {
-					// end of options detected
-					return nil
-				}
-			default:
-				optionDelta += int(od)
-
+		// figure out Option Delta
+		switch od {
+		case 13:
+			i++
+			if len(buffer) < i+1 {
+				return i, PacketIsTooShort
 			}
+			optionDelta += int(buffer[i])
+			i++
 
-			// figure out Option Length
-			switch ol {
-
-			case 13:
-				i++
-				if len(buffer) < i+1 {
-					return PacketIsTooShort
-				}
-				optionLength += int(buffer[i])
-				i++
-
-			case 14:
-				i++
-				if len(buffer) < i+2 {
-					return PacketIsTooShort
-				}
-				optionLength = int(binary.BigEndian.Uint16(buffer[i:i+2]) - 269)
-				i += 2
-
-			case 15:
-				// invalid value
-				return MessageFormatError
-
-			default:
-				optionLength = int(ol)
-				i++
+		case 14:
+			i++
+			if len(buffer) < i+2 {
+				return i, PacketIsTooShort
 			}
+			optionDelta += int(binary.BigEndian.Uint16(buffer[i:i+2]) + 269)
+			i += 2
 
-			if len(buffer) < i+optionLength {
-				return PacketIsTooShort
+		case 15:
+			// whole byte should be 0xFF, otherwise error
+			if ol != 0xF {
+				return i, MessageFormatError
+			} else {
+				// end of options detected
+				return i, nil
 			}
-			optionValue = make([]byte, optionLength)
-			copy(optionValue, buffer[i:i+optionLength])
-			fmt.Printf("optionDelta: %v, optionLength: %v, i: %v, optionValue: %s\n", optionDelta, optionLength, i, hex.Dump(optionValue))
+		default:
+			optionDelta += int(od)
 		}
+
+		// figure out Option Length
+		switch ol {
+
+		case 13:
+			i++
+			if len(buffer) < i+1 {
+				return i, PacketIsTooShort
+			}
+			optionLength += int(buffer[i])
+			i++
+
+		case 14:
+			i++
+			if len(buffer) < i+2 {
+				return i, PacketIsTooShort
+			}
+			optionLength = int(binary.BigEndian.Uint16(buffer[i:i+2]) - 269)
+			i += 2
+
+		case 15:
+			// invalid value
+			return i, MessageFormatError
+
+		default:
+			optionLength = int(ol)
+			i++
+		}
+
+		if len(buffer) < i+optionLength {
+			return i, PacketIsTooShort
+		}
+		optionValue = make([]byte, optionLength)
+		copy(optionValue, buffer[i:i+optionLength])
+		i += optionLength
 	}
-	return nil
+	return i, nil
 }
--- message/message_token.go
+++ message/message_token.go
@@ -12,7 +12,7 @@ func (t TokenType) String() string {
 	var b bytes.Buffer
 	b.WriteString("0x")
 	for _, i := range t {
-		b.WriteString(fmt.Sprintf("%02x", i))
+		b.WriteString(fmt.Sprintf("%02X", i))
 	}
 	return b.String()
 }
