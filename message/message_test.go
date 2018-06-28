8991c36d4e97ddab2a32f855ffbc4b28c183285c
Alex Ellwein <alex@etero.net>
2018-06-28 23:05:08 +0200

    adjust test

--- message/message_test.go
+++ message/message_test.go
@@ -1,7 +1,6 @@
 package message
 
 import (
-	"fmt"
 	"github.com/aellwein/coap/util"
 	c "github.com/smartystreets/goconvey/convey"
 	"strings"
@@ -129,7 +128,7 @@ func TestOptionsAreParsedCorrectly(t *testing.T) {
 
 			c.Convey("Then decode should not have errors", func() {
 				c.So(err, c.ShouldBeNil)
-				fmt.Printf("%v", m)
+				c.So(m.String(), c.ShouldNotBeNil)
 			})
 			c.Convey("And message should have 4 options", func() {
 				c.So(len(*m.Options), c.ShouldEqual, 4)
