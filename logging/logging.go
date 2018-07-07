0751c1f9714795adcb741bc396a82e9956629568
Alex Ellwein <alex@etero.net>
2018-07-07 17:50:54 +0200

    use slf4go adaptor

--- logging/logging.go
+++ logging/logging.go
@@ -1,18 +1,3 @@
 package logging
 
-import (
-	"github.com/aellwein/slf4go"
-	"go.uber.org/zap"
-)
-
-var LoggerFactory slf4go.LoggerFactory
-
-// log4go facade initialization
-func init() {
-	logger, err := zap.NewDevelopment()
-	if err != nil {
-		panic(err)
-	}
-	LoggerFactory = NewZapLoggerFactory(logger)
-	slf4go.SetLoggerFactory(LoggerFactory)
-}
+import _ "github.com/aellwein/slf4go-zap-adaptor"
