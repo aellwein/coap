bdaeec5680c43600fb5f09d6b51f5e45dedf6a1a
Alex Ellwein <alex@etero.net>
2018-07-12 22:15:42 +0200

    refactored

--- request.go
+++ request.go
@@ -1,27 +1,8 @@
 package coap
 
-import (
-	"fmt"
-	"github.com/aellwein/coap/message"
-)
+import "github.com/aellwein/coap/message"
 
+// Request describes an incoming request.
 type Request interface {
-	Message() message.Message
-}
-
-type RequestHandler struct {
-	Path         string
-	HandlePOST   func(r Request) error
-	HandlePUT    func(r Request) error
-	HandleGET    func(r Request) error
-	HandleDELETE func(r Request) error
-}
-
-func (r RequestHandler) String() string {
-	return fmt.Sprintf("RequestHandler{ path=%s, HandlePOST=%p, HandlePUT=%p, HandleGET=%p, HandleDELETE=%p }",
-		r.Path,
-		r.HandlePOST,
-		r.HandlePUT,
-		r.HandleGET,
-		r.HandleDELETE)
+	GetReq() *message.Message
 }
