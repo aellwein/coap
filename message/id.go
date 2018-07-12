4e0156f99b092a9e44f30ce80d2a84d485fcb9be
Alex Ellwein <alex@etero.net>
2018-07-12 18:28:08 +0200

    add func to create new message ID

--- message/id.go
+++ message/id.go
@@ -1,10 +1,17 @@
 package message
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 // Message ID, 16 bits.
 type MessageIdType uint16
 
 func (m MessageIdType) String() string {
 	return fmt.Sprintf("0x%04X", uint16(m))
+}
+
+func NewMessageId() MessageIdType {
+	return MessageIdType(rand.Uint32())
 }
