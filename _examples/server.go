815f4e3c7742f9f9aa9e934d09216fbc4654669c
Alex Ellwein <alex@etero.net>
2018-07-15 15:10:13 +0200

    add example server which mimics a lwm2m server

--- _examples/server.go
+++ _examples/server.go
@@ -6,14 +6,73 @@ import (
 	_ "github.com/aellwein/slf4go-zap-adaptor"
 )
 
-func main() {
+var (
+	server *coap.Server
+	logger slf4go.Logger
+)
+
+func init() {
 	slf4go.GetLoggerFactory().SetDefaultLogLevel(slf4go.LevelDebug)
 	slf4go.GetLoggerFactory().SetLoggingParameters(slf4go.LoggingParameters{
 		"development": true,
 	})
-	logger := slf4go.GetLogger("server")
+	logger = slf4go.GetLogger("server")
+}
 
-	server, err := coap.NewInsecureCoapServerWithDefaultParameters(coap.NewResource("/rd"))
+func onRegister(request *coap.Message) (*coap.Message, error) {
+
+	logger.Debugf("onRegister(%v)", request)
+
+
+	server.AddResource(&coap.Resource{
+		Path:   "/rd/cafe/babe",
+		OnPOST: OnUpdate,
+		OnDELETE: OnDelete,
+	})
+
+	return coap.NewAcknowledgementMessageBuilder().
+		Code(coap.Created).
+		MessageId(request.MessageID).
+		Token(request.Token).
+		Option(coap.LocationPath, coap.NewLocationPathOption("/rd/cafe/babe")...).
+		Build(), nil
+
+}
+
+func OnUpdate(request *coap.Message) (*coap.Message, error) {
+
+	logger.Debugf("onUpdate(%v)", request)
+
+	return coap.NewAcknowledgementMessageBuilder().
+		Code(coap.Changed).
+		MessageId(request.MessageID).
+		Token(request.Token).
+		Option(coap.LocationPath, coap.NewLocationPathOption("/rd/cafe/babe")...).
+		Build(), nil
+}
+
+func OnDelete(request *coap.Message) (*coap.Message, error) {
+
+	logger.Debugf("onDelete(%v)", request)
+
+	server.RemoveResourceByPath("/rd/cafe/babe")
+
+	return coap.NewAcknowledgementMessageBuilder().
+		Code(coap.Deleted).
+		MessageId(request.MessageID).
+		Token(request.Token).
+		Option(coap.LocationPath, coap.NewLocationPathOption("/rd/cafe/babe")...).
+		Build(), nil
+}
+
+func main() {
+	var err error
+	server, err = coap.NewInsecureCoapServerWithDefaultParameters(
+		&coap.Resource{
+			Path:   "/rd",
+			OnPOST: onRegister,
+		},
+	)
 	if err != nil {
 		logger.Panic(err)
 	}
