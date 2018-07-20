420a3b2995c597cda2fbf83416906e9825a7428f
Alex Ellwein <alex@etero.net>
2018-07-20 22:45:46 +0200

    simplify numeric testing

--- numeric_test.go
+++ numeric_test.go
@@ -1,200 +1,132 @@
 package coap
 
 import (
-	"encoding/binary"
+	"fmt"
 	c "github.com/smartystreets/goconvey/convey"
+	"reflect"
 	"testing"
 )
 
-func TestToBigEndianNumberOfOneByte(t *testing.T) {
-	c.Convey("Given a byte array with 1 element (0x20)", t, func() {
-		var b = []byte{0x20}
-		c.Convey("When converted to big endian number", func() {
-			n := ToBigEndianNumber(b)
-			c.Convey("Result must be 0x20", func() {
-				c.So(n.(byte), c.ShouldEqual, 0x20)
-			})
-		})
-	})
+type testMatrix struct {
+	input        []byte
+	Type         reflect.Type
+	bigEndian    interface{}
+	littleEndian interface{}
 }
 
-func TestToLittleEndianNumberOfOneByte(t *testing.T) {
-	c.Convey("Given a byte array with 1 element (0x20)", t, func() {
-		var b = []byte{0x20}
-		c.Convey("When converted to little endian number", func() {
-			n := ToLittleEndianNumber(b)
-			c.Convey("Result must be 0x20", func() {
-				c.So(n.(byte), c.ShouldEqual, 0x20)
+var (
+	tByte   byte
+	tUint16 uint16
+	tUint32 uint32
+	tUint64 uint64
+
+	tests = []testMatrix{
+		{
+			input:        []byte{0x20},
+			Type:         reflect.TypeOf(tByte),
+			bigEndian:    0x20,
+			littleEndian: 0x20,
+		},
+		{
+			input:        []byte{0x13, 0x37},
+			Type:         reflect.TypeOf(tUint16),
+			bigEndian:    0x1337,
+			littleEndian: 0x3713,
+		},
+		{
+			input:        []byte{0xCA, 0xFE, 0xBA},
+			Type:         reflect.TypeOf(tUint32),
+			bigEndian:    0xCAFEBA,
+			littleEndian: 0x00BAFECA,
+		},
+		{
+			input:        []byte{0xCA, 0xFE, 0xBA, 0xBE},
+			Type:         reflect.TypeOf(tUint32),
+			bigEndian:    0xCAFEBABE,
+			littleEndian: 0xBEBAFECA,
+		},
+		{
+			input:        []byte{0xCA, 0xFE, 0xBA, 0xBE, 0xDE},
+			Type:         reflect.TypeOf(tUint64),
+			bigEndian:    0xCAFEBABEDE,
+			littleEndian: 0xDEBEBAFECA,
+		},
+		{
+			input:        []byte{0xCA, 0xFE, 0xBA, 0xBE, 0xDE, 0xAD},
+			Type:         reflect.TypeOf(tUint64),
+			bigEndian:    0xCAFEBABEDEAD,
+			littleEndian: 0xADDEBEBAFECA,
+		},
+		{
+			input:        []byte{0xCA, 0xFE, 0xBA, 0xBE, 0xDE, 0xAD, 0xBE},
+			Type:         reflect.TypeOf(tUint64),
+			bigEndian:    0xCAFEBABEDEADBE,
+			littleEndian: 0xBEADDEBEBAFECA,
+		},
+		{
+			input:        []byte{0xCA, 0xFE, 0xBA, 0xBE, 0xDE, 0xAD, 0xBE, 0xEF},
+			Type:         reflect.TypeOf(tUint64),
+			bigEndian:    uint64(0xCAFEBABEDEADBEEF),
+			littleEndian: uint64(0xEFBEADDEBEBAFECA),
+		},
+		{
+			input:        []byte{0xCA, 0xFE, 0xBA, 0xBE, 0xDE, 0xAD, 0xBE, 0xEF, 0x13, 0x37},
+			Type:         reflect.TypeOf(tUint64),
+			bigEndian:    uint64(0xBABEDEADBEEF1337),
+			littleEndian: uint64(0xEFBEADDEBEBAFECA),
+		},
+	}
+)
+
+func TestToNumber_All(t *testing.T) {
+	c.Convey("Given a byte array representing number", t, func() {
+		for _, test := range tests {
+
+			c.Convey(fmt.Sprintf("When converting %v to big endian number", HexContent(test.input)), func() {
+
+				c.Convey(fmt.Sprintf("And array consists of %d bytes", len(test.input)), func() {
+
+					n, _ := ToBigEndianNumber(test.input)
+
+					c.Convey(fmt.Sprintf("Then resulting type is '%v'", test.Type), func() {
+						c.So(reflect.TypeOf(n), c.ShouldEqual, test.Type)
+
+						c.Convey("And the result equals the expected number", func() {
+							c.So(n, c.ShouldEqual, test.bigEndian)
+						})
+					})
+				})
+			})
+			c.Convey(fmt.Sprintf("When converting %v to little endian number", HexContent(test.input)), func() {
+				c.Convey(fmt.Sprintf("And array consists of %d bytes", len(test.input)), func() {
+
+					n, _ := ToLittleEndianNumber(test.input)
+
+					c.Convey(fmt.Sprintf("Then resulting type is '%v'", test.Type), func() {
+						c.So(reflect.TypeOf(n), c.ShouldEqual, test.Type)
+
+						c.Convey("And the result equals the expected number", func() {
+							c.So(n, c.ShouldEqual, test.littleEndian)
+						})
+					})
+				})
+
+			})
+		}
+
+		// special cases
+
+		c.Convey("When a byte array of zero length is converted to big endian number", func() {
+			_, err := ToBigEndianNumber([]byte{})
+			c.Convey("Then an error is returned", func() {
+				c.So(err, c.ShouldNotBeNil)
 			})
 		})
-	})
-}
 
-func TestToBigEndianNumberOfTwoBytes(t *testing.T) {
-	c.Convey("Given a big endian number of 2 bytes", t, func() {
-		b := make([]byte, 2)
-		binary.BigEndian.PutUint16(b, 5683)
-
-		c.Convey("When converted to big endian number", func() {
-			n := ToBigEndianNumber(b)
-
-			c.Convey("Then the result must be equal the given number", func() {
-				c.So(n.(uint16), c.ShouldEqual, 5683)
-			})
-		})
-	})
-}
-
-func TestToLittleEndianNumberOfTwoBytes(t *testing.T) {
-	c.Convey("Given a little endian number of 2 bytes", t, func() {
-		b := make([]byte, 2)
-		binary.LittleEndian.PutUint16(b, 5683)
-
-		c.Convey("When converted to little endian number", func() {
-			n := ToLittleEndianNumber(b)
-
-			c.Convey("Then the result must be equal the given number", func() {
-				c.So(n.(uint16), c.ShouldEqual, 5683)
-			})
-		})
-	})
-}
-
-func TestToBigEndianNumberOfThreeBytes(t *testing.T) {
-	c.Convey("Given a big endian number of 3 bytes", t, func() {
-		b := []byte{0x00, 0x16, 0x33}
-
-		c.Convey("When converted to big endian number", func() {
-			n := ToBigEndianNumber(b)
-
-			c.Convey("Then the result must be equal the given number", func() {
-				c.So(n.(uint32), c.ShouldEqual, 5683)
-			})
-		})
-	})
-}
-
-func TestToLittleEndianNumberOfThreeBytes(t *testing.T) {
-	c.Convey("Given a little endian number of 3 bytes", t, func() {
-		b := []byte{0x33, 0x16, 0x00}
-
-		c.Convey("When converted to little endian number", func() {
-			n := ToLittleEndianNumber(b)
-
-			c.Convey("Then the result must be equal the given number", func() {
-				c.So(n.(uint32), c.ShouldEqual, 5683)
-			})
-		})
-	})
-}
-
-func TestToBigEndianNumberOfFourBytes(t *testing.T) {
-	c.Convey("Given a big endian number of 4 bytes", t, func() {
-		b := []byte{0xCA, 0xFE, 0xBA, 0xBE}
-
-		c.Convey("When converted to big endian number", func() {
-			n := ToBigEndianNumber(b)
-
-			c.Convey("Then the result must be equal the given number", func() {
-				c.So(n.(uint32), c.ShouldEqual, 0xCAFEBABE)
-			})
-		})
-	})
-}
-
-func TestToLittleEndianNumberOfFourBytes(t *testing.T) {
-	c.Convey("Given a little endian number of 4 bytes", t, func() {
-		b := []byte{0xBE, 0xBA, 0xFE, 0xCA}
-
-		c.Convey("When converted to little endian number", func() {
-			n := ToLittleEndianNumber(b)
-
-			c.Convey("Then the result must be equal the given number", func() {
-				c.So(n.(uint32), c.ShouldEqual, 0xCAFEBABE)
-			})
-		})
-	})
-}
-
-func TestToBigEndianNumberOfSixBytes(t *testing.T) {
-	c.Convey("Given a big endian number of 6 bytes", t, func() {
-		b := []byte{0x00, 0x00, 0xCA, 0xFE, 0xBA, 0xBE}
-
-		c.Convey("When converted to big endian number", func() {
-			n := ToBigEndianNumber(b)
-
-			c.Convey("Then the result must be equal the given number", func() {
-				c.So(n.(uint64), c.ShouldEqual, 0xCAFEBABE)
-			})
-		})
-	})
-}
-
-func TestToLittleEndianNumberOfSixBytes(t *testing.T) {
-	c.Convey("Given a little endian number of 6 bytes", t, func() {
-		b := []byte{0xBE, 0xBA, 0xFE, 0xCA, 0x00, 0x00}
-
-		c.Convey("When converted to little endian number", func() {
-			n := ToLittleEndianNumber(b)
-
-			c.Convey("Then the result must be equal the given number", func() {
-				c.So(n.(uint64), c.ShouldEqual, 0xCAFEBABE)
-			})
-		})
-	})
-}
-
-func TestToBigEndianNumberOfEightBytes(t *testing.T) {
-	c.Convey("Given a big endian number of 8 bytes", t, func() {
-		b := []byte{0xCA, 0xFE, 0xBA, 0xBE, 0xDE, 0xAD, 0xBE, 0xEF}
-
-		c.Convey("When converted to big endian number", func() {
-			n := ToBigEndianNumber(b)
-
-			c.Convey("Then the result must be equal the given number", func() {
-				c.So(n.(uint64), c.ShouldEqual, uint64(0xCAFEBABEDEADBEEF))
-			})
-		})
-	})
-}
-
-func TestToLittleEndianNumberOfEightBytes(t *testing.T) {
-	c.Convey("Given a little endian number of 8 bytes", t, func() {
-		b := []byte{0xEF, 0xBE, 0xAD, 0xDE, 0xBE, 0xBA, 0xFE, 0xCA}
-
-		c.Convey("When converted to little endian number", func() {
-			n := ToLittleEndianNumber(b)
-
-			c.Convey("Then the result must be equal the given number", func() {
-				c.So(n.(uint64), c.ShouldEqual, uint64(0xCAFEBABEDEADBEEF))
-			})
-		})
-	})
-}
-
-func TestToBigEndianNumberOfTenBytes(t *testing.T) {
-	c.Convey("Given a big endian number of 10 bytes", t, func() {
-		b := []byte{0x00, 0x00, 0xCA, 0xFE, 0xBA, 0xBE, 0xDE, 0xAD, 0xBE, 0xEF}
-
-		c.Convey("When converted to big endian number", func() {
-			n := ToBigEndianNumber(b)
-
-			c.Convey("Then the result must be equal the boxed uint64 value", func() {
-				c.So(n.(uint64), c.ShouldEqual, uint64(0xCAFEBABEDEADBEEF))
-			})
-		})
-	})
-}
-
-func TestToLittleEndianNumberOfTenBytes(t *testing.T) {
-	c.Convey("Given a little endian number of 10 bytes", t, func() {
-		b := []byte{0xEF, 0xBE, 0xAD, 0xDE, 0xBE, 0xBA, 0xFE, 0xCA, 0x00, 0x00}
-
-		c.Convey("When converted to little endian number", func() {
-			n := ToLittleEndianNumber(b)
-
-			c.Convey("Then the result must be equal the given number", func() {
-				c.So(n.(uint64), c.ShouldEqual, uint64(0xCAFEBABEDEADBEEF))
+		c.Convey("When a byte array of zero length is converted to little endian number", func() {
+			_, err := ToLittleEndianNumber([]byte{})
+			c.Convey("Then an error is returned", func() {
+				c.So(err, c.ShouldNotBeNil)
 			})
 		})
 	})
