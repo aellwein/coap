c49173c2df298b937ae196c149c2f7daeb4877f6
Alex Ellwein <alex@etero.net>
2018-07-21 00:06:02 +0200

    add String() for Resource type

--- resource.go
+++ resource.go
@@ -1,5 +1,7 @@
 package coap
 
+import "fmt"
+
 // ResourceHandlerFunc is a shortcut type for resource handling function.
 type ResourceHandlerFunc func(request *Message) (*Message, error)
 
@@ -9,4 +11,9 @@ type Resource struct {
 	OnPUT    ResourceHandlerFunc
 	OnPOST   ResourceHandlerFunc
 	OnDELETE ResourceHandlerFunc
+}
+
+func (r *Resource) String() string {
+	return fmt.Sprintf("Resource{ Path: '%v', OnGET: %v, OnPUT: %v, OnPOST: %v, OnDELETE: %v }",
+		r.Path, r.OnGET, r.OnPUT, r.OnPOST, r.OnDELETE)
 }
