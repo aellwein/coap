f2643eb84d88da3e04df4097a696332828c5889a
Alex Ellwein <alex@etero.net>
2018-07-20 22:45:26 +0200

    return error in case of failed numeric conversion

--- numeric.go
+++ numeric.go
@@ -2,6 +2,7 @@ package coap
 
 import (
 	"encoding/binary"
+	"errors"
 )
 
 func padBE(b []byte, targetLen int) []byte {
@@ -16,42 +17,46 @@ func padLE(b []byte, targetLen int) []byte {
 	return target
 }
 
-func ToBigEndianNumber(b []byte) interface{} {
+func ToBigEndianNumber(b []byte) (interface{}, error) {
 	switch len(b) {
+	case 0:
+		return nil, errors.New("number of zero length is invalid")
 	case 1:
-		return b[0]
+		return b[0], nil
 	case 2:
-		return binary.BigEndian.Uint16(b)
+		return binary.BigEndian.Uint16(b), nil
 	case 3:
-		return binary.BigEndian.Uint32(padBE(b, 4))
+		return binary.BigEndian.Uint32(padBE(b, 4)), nil
 	case 4:
-		return binary.BigEndian.Uint32(b)
+		return binary.BigEndian.Uint32(b), nil
 	case 5, 6, 7:
-		return binary.BigEndian.Uint64(padBE(b, 8))
+		return binary.BigEndian.Uint64(padBE(b, 8)), nil
 	case 8:
-		return binary.BigEndian.Uint64(b)
+		return binary.BigEndian.Uint64(b), nil
 	default:
 		// everything above 8 bytes gets boxed into 8 bytes
-		return binary.BigEndian.Uint64(b[len(b)-8:])
+		return binary.BigEndian.Uint64(b[len(b)-8:]), nil
 	}
 }
 
-func ToLittleEndianNumber(b []byte) interface{} {
+func ToLittleEndianNumber(b []byte) (interface{}, error) {
 	switch len(b) {
+	case 0:
+		return nil, errors.New("number of zero length is invalid")
 	case 1:
-		return b[0]
+		return b[0], nil
 	case 2:
-		return binary.LittleEndian.Uint16(b)
+		return binary.LittleEndian.Uint16(b), nil
 	case 3:
-		return binary.LittleEndian.Uint32(padLE(b, 4))
+		return binary.LittleEndian.Uint32(padLE(b, 4)), nil
 	case 4:
-		return binary.LittleEndian.Uint32(b)
+		return binary.LittleEndian.Uint32(b), nil
 	case 5, 6, 7:
-		return binary.LittleEndian.Uint64(padLE(b, 8))
+		return binary.LittleEndian.Uint64(padLE(b, 8)), nil
 	case 8:
-		return binary.LittleEndian.Uint64(b)
+		return binary.LittleEndian.Uint64(b), nil
 	default:
 		// everything above 8 bytes gets boxed into 8 bytes
-		return binary.LittleEndian.Uint64(b[0:8])
+		return binary.LittleEndian.Uint64(b[0:8]), nil
 	}
 }
